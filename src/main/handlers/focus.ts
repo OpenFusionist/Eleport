186536d50b461b889b05e1348690e60d42c05a66
0ximDean <imdean@proton.me>
2025-05-08 12:00:14 +0800

    bugfix

--- src/main/handlers/focus.ts
+++ src/main/handlers/focus.ts
@@ -1,7 +1,12 @@
 import { mainWindow } from "..";
 
 export function windowFocus() {
-  if(!mainWindow) return
-  if (mainWindow.isMinimized()) mainWindow.restore();
-    mainWindow.focus();
+  try{
+    if(!mainWindow) return
+    if (mainWindow.isMinimized()) mainWindow.restore();
+      mainWindow.focus();
+  } catch(e){
+    console.error('windowFocus error:', e);
+    // Sentry.captureException(e);
+  }
 }
