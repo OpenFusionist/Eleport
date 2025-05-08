e11ffb2868f083d097527b941936bb51fc7b0db1
0ximDean <imdean@proton.me>
2025-05-08 14:16:32 +0800

    Revert "bugfix"
    This reverts commit 186536d50b461b889b05e1348690e60d42c05a66.

--- src/main/handlers/focus.ts
+++ src/main/handlers/focus.ts
@@ -1,12 +1,7 @@
 import { mainWindow } from "..";
 
 export function windowFocus() {
-  try{
-    if(!mainWindow) return
-    if (mainWindow.isMinimized()) mainWindow.restore();
-      mainWindow.focus();
-  } catch(e){
-    console.error('windowFocus error:', e);
-    // Sentry.captureException(e);
-  }
+  if(!mainWindow) return
+  if (mainWindow.isMinimized()) mainWindow.restore();
+    mainWindow.focus();
 }
