90d43370933f112c757cbce722a0bc5ec3c93a1e
0ximDean <imdean@proton.me>
2025-09-02 17:32:02 +0800

    improve game startup

--- src/main/handlers/play.ts
+++ src/main/handlers/play.ts
@@ -1,12 +1,12 @@
 
-import { exec } from 'child_process';
+import fs from 'fs/promises';
 import { GetGameDownloadDir, getProcessList } from './../utils';
 import path from 'path';
 import { mainWindow } from '..';
 
 import * as Sentry from "@sentry/electron/main";
 import { GAME_PROCESS_NAME } from '../configs';
-import { dialog } from 'electron';
+import { shell, dialog } from 'electron';
 
 
 export async function play() {
@@ -23,16 +23,30 @@ export async function play() {
   }
 
   const exePath = path.join(GetGameDownloadDir(), 'fusionist.exe'); //GetGameDownloadDir() + "fusionist.exe"// path.join(__dirname, 'your-executable.exe'); 
+  
+  try { await fs.access(exePath); }
+  catch {
+    await dialog.showMessageBox({ type: "error", message: `Game executable not found:\n${exePath}`});
+    return;
+  }
+  
   if(mainWindow && !mainWindow.isDestroyed()) {
     mainWindow.minimize();
   }
-  exec(`"${exePath}"`, (error, stdout, stderr) => {
-    if (error) {
-      Sentry.captureException(error);
-      console.error(`exec error: ${error}`);
-      return;
-    }
-    console.log(`stdout: ${stdout}`);
-    console.error(`stderr: ${stderr}`);
-  });
+  
+  // exec(`"${exePath}"`, (error, stdout, stderr) => {
+  //   if (error) {
+  //     Sentry.captureException(error);
+  //     console.error(`exec error: ${error}`);
+  //     return;
+  //   }
+  //   console.log(`stdout: ${stdout}`);
+  //   console.error(`stderr: ${stderr}`);
+  // });
+
+  const err = await shell.openPath(exePath);
+  if (err) {
+    Sentry.captureMessage(`shell.openPath failed: ${err}`);
+    await dialog.showMessageBox({ type: "error", message: `Failed to launch game:\n${err}` });
+  }
 }
